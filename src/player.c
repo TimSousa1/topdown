57f85f2c26eb8efb6a0756534d7503e396b274c0
TimSousa <martimnevessousa@gmail.com>
2024-07-15 23:36:30 +0100

    added basic player movement and .clangd file

--- src/player.c
+++ src/player.c
@@ -1,4 +1,6 @@
 #include "player.h"
+#include <raylib.h>
+#include <raymath.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -16,4 +18,15 @@ void print_player(Player player) {
     printf("pos:(%f, %f)\nspeed:(%f, %f)\nsize:(%f)\npointer_pos:(%f, %f)\npointer_size:(%f)\nmovespeed:(%f)\ndir:(%f,%f)\n",
             player.pos.x, player.pos.y, player.speed.x, player.speed.y, player.size, player.pointer_pos.x, player.pointer_pos.y,
             player.pointer_size, player.movespeed, player.dir.x, player.dir.y);
+}
+
+Vector2 get_move_dir(void) {
+    Vector2 move_dir = {0};
+    move_dir.y -= IsKeyDown(KEY_W);
+    move_dir.x -= IsKeyDown(KEY_A);
+    move_dir.y += IsKeyDown(KEY_S);
+    move_dir.x += IsKeyDown(KEY_D);
+
+    return Vector2Normalize(move_dir);
+
 }
