fc38b4c213b3d39de82db478e0e339200c6d1ca6
TimSousa <martimnevessousa@gmail.com>
2024-08-16 17:22:13 +0100

    player disconnect crash fix and not render

--- src/server/server.c
+++ src/server/server.c
@@ -1,7 +1,8 @@
 // game
 #include "game.h"
-#include "player.h"
 #include "packets.h"
+#include "player.h"
+#include "server.h"
 
 // stdlib
 #include <netinet/in.h>
@@ -22,17 +23,18 @@
 
 #include <fcntl.h>
 
-
 #define TICK_RATE 128
 
 struct con_t {
     int sock_fd;
     struct sockaddr sock;
     char ip[INET_ADDRSTRLEN];
+    int last_packet_timestamp; // number of ticks since last received packet from
+                               // connection
 };
 
 struct con_t setup_net(char *server_ip) {
-    struct con_t server = {0}; // return value
+    struct con_t server = {0};
     server.sock_fd = -1;
 
     struct addrinfo hints, *res;
@@ -128,25 +130,28 @@ int main(int argc, char **argv) {
     for (int i = 0; i < ROOM_SIZE; i++) {
         init_player(&players[i], get_player_color(i));
         players[i].id = i;
-        players[i].pos = (Vector2){ (float) (i +.5) * WORLD_W / (ROOM_SIZE), (float)WORLD_H / 2}; // will depend on each map..
+        players[i].pos = (Vector2){(float)(i + .5) * WORLD_W / (ROOM_SIZE),
+            (float)WORLD_H / 2}; // will depend on each map..
 
         initial.players[i].pos = players[i].pos;
     }
 
-    // initial id's have to start at 1 since -0 doesn't exist (it does, but you get me :) )
+    // initial id's have to start at 1 since -0 doesn't exist (it does, but you
+    // get me :) )
     printf("----- PACKET -----\n");
     for (int i = 0; i < ROOM_SIZE; i++) {
-        initial.players[i].id = -(i+1); // a negative id tells the connection what it's index is on the server
-        printf("id:%d pos:(%f, %f)\n", initial.players[i].id, initial.players[i].pos.x, initial.players[i].pos.y);
+        initial.players[i].id = -(i + 1); // a negative id tells the connection what
+                                          // it's index is on the server
+        printf("id:%d pos:(%f, %f)\n", initial.players[i].id,
+                initial.players[i].pos.x, initial.players[i].pos.y);
         send(cons[i].sock_fd, &initial, sizeof(initial), 0);
-        initial.players[i].id = i+1;
+        initial.players[i].id = i + 1;
     }
 
     // resetting id's to their original value
     for (int i = 0; i < ROOM_SIZE; i++) {
         initial.players[i].id = i;
     }
-
 
     printf("----- PLAYERS -----\n");
     for (int i = 0; i < ROOM_SIZE; i++) {
@@ -157,47 +162,62 @@ int main(int argc, char **argv) {
     packet_input pack_in[ROOM_SIZE] = {0};
     packet_output pack_out = {0};
 
-    for (int i = 0; i < ROOM_SIZE; i++) fcntl(cons[i].sock_fd, F_SETFL, O_NONBLOCK);
+    for (int i = 0; i < ROOM_SIZE; i++) {
+        fcntl(cons[i].sock_fd, F_SETFL, O_NONBLOCK);
+    }
 
     struct timespec diff = {0};
-    while(1){
+    while (1) {
+
         struct timespec start, end, to_sleep;
         clock_gettime(CLOCK_MONOTONIC, &start);
 
+        for (int i = 0; i < ROOM_SIZE; i++)
+            cons[i].last_packet_timestamp += 1;
+
         for (int i = 0; i < ROOM_SIZE; i++) {
-            while (recv(cons[i].sock_fd, &pack_in[i], sizeof(pack_in[i]), 0) > 0);
+            while (recv(cons[i].sock_fd, &pack_in[i], sizeof(pack_in[i]), 0) > 0) {
+                cons[i].last_packet_timestamp = 0;
+            }
             // if (i == 0) {printv2(pack_in[i].move_dir);puts("");}
         }
 
         for (int i = 0; i < ROOM_SIZE; i++) {
             players[i].speed = Vector2Scale(pack_in[i].player.move_dir, players[i].movespeed);
-            players[i].pos = Vector2Add(players[i].pos, Vector2Scale(players[i].speed, diff.tv_sec + diff.tv_nsec/1e9));
+            players[i].pos = Vector2Add(players[i].pos, Vector2Scale(players[i].speed, diff.tv_sec + diff.tv_nsec / 1e9));
             players[i].dir = pack_in[i].player.dir;
 
+            pack_out.players[i].id = players[i].id;
             pack_out.players[i].pos = players[i].pos;
             pack_out.players[i].dir = players[i].dir;
         }
 
         for (int i = 0; i < ROOM_SIZE; i++) {
-            send(cons[i].sock_fd, &pack_out, sizeof(pack_out), 0);
+            send(cons[i].sock_fd, &pack_out, sizeof(pack_out), MSG_NOSIGNAL);
+            if (cons[i].last_packet_timestamp > TICK_RATE * TIMEOUT_SECS) {
+                players[i].id *= -1; // player i has disconnected
+            }
         }
 
+        // tickrate sleep
         clock_gettime(CLOCK_MONOTONIC, &end);
-        diff.tv_sec = end.tv_sec - start.tv_sec;      // TODO: change diff time calculation
+        diff.tv_sec =
+            end.tv_sec - start.tv_sec; // TODO: change diff time calculation
         diff.tv_nsec = end.tv_nsec - start.tv_nsec;
 
         to_sleep.tv_sec = 0 - diff.tv_sec;
-        to_sleep.tv_nsec = 1e9/TICK_RATE - diff.tv_nsec;
+        to_sleep.tv_nsec = 1e9 / TICK_RATE - diff.tv_nsec;
 
-        if (to_sleep.tv_sec < 0 || to_sleep.tv_nsec < 0) {/*printf("not sleeping!\n");*/ continue;}
-        nanosleep(&to_sleep, NULL); 
+        if (to_sleep.tv_sec < 0 || to_sleep.tv_nsec < 0) { /*printf("not sleeping!\n");*/
+            continue;
+        }
+        nanosleep(&to_sleep, NULL);
         // printf("sleeping!\n");
 
         clock_gettime(CLOCK_MONOTONIC, &end);
         diff.tv_sec = end.tv_sec - start.tv_sec;
         diff.tv_nsec = end.tv_nsec - start.tv_nsec;
     }
-
 
     return 0;
 }
